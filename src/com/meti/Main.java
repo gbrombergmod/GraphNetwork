474974cf5359620344dac37940c452af98823d44
SirMathhman <sirmathhman@gmail.com>
2023-05-05 15:37:55 -0400

    Introduce chunks

--- src/com/meti/Main.java
+++ src/com/meti/Main.java
@@ -1,37 +1,44 @@
 package com.meti;
 
 import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    public static final double LEARNING_RATE = 0.0001;
+    public static final double LEARNING_RATE = 0.1;
+    public static final int CHUNK_COUNT = 10;
+    public static final BinaryOperator<Node> CHOOSE_RIGHT = (previous, next) -> next;
 
     public static void main(String[] args) {
         var data = IntStream.range(0, 1000)
                 .boxed()
                 .collect(Collectors.toMap(Function.identity(), value -> value % 2 == 0));
 
-        var weight = Math.random();
-        var bias = Math.random();
-        var node = new Node(weight, bias);
+        var chunks = data.entrySet()
+                .stream()
+                .collect(Collectors.groupingBy(entry -> entry.getKey() % CHUNK_COUNT))
+                .values();
 
-        var gradientSum = data.entrySet().stream().reduce(Node.zero(), (gradientSum1, entry) -> {
-            var normalized = normalize(data, entry);
-            var activated = forward(node, normalized);
+        var trained = chunks.stream().reduce(Node.random(), (node, entries) -> {
+            var gradientSum = data.entrySet().stream().reduce(Node.zero(), (gradientSum1, entry) -> {
+                var normalized = normalize(data, entry);
+                var activated = forward(node, normalized);
 
-            var isEven = entry.getValue();
-            var expected = isEven ? 0d : 1d;
-            var cost = 2d * (activated - expected);
-            var activatedDerivative = activated * (1 - activated);
-            var baseDerivative = cost * activatedDerivative;
-            var gradient = new Node(normalized, 1d).multiply(baseDerivative);
-            return gradientSum1.add(gradient);
-        }, (previous, next) -> next);
+                var isEven = entry.getValue();
+                var expected = isEven ? 0d : 1d;
+                var cost = 2d * (activated - expected);
+                var activatedDerivative = activated * (1 - activated);
+                var baseDerivative = cost * activatedDerivative;
+                var gradient = new Node(normalized, 1d).multiply(baseDerivative);
+                return gradientSum1.add(gradient);
+            }, CHOOSE_RIGHT);
 
-        var trained = node.subtract(gradientSum.divide(data.size()).multiply(LEARNING_RATE));
+            System.out.println(node);
+            return node.subtract(gradientSum.divide(data.size()).multiply(LEARNING_RATE));
+        }, CHOOSE_RIGHT);
 
         var totalCorrect = data.entrySet().stream().mapToInt(entry -> {
             var normalized = normalize(data, entry);
--- src/com/meti/Node.java
+++ src/com/meti/Node.java
@@ -5,6 +5,12 @@ public record Node(double weight, double bias) {
         return new Node(0, 0);
     }
 
+    static Node random() {
+        var weight = Math.random();
+        var bias = Math.random();
+        return new Node(weight, bias);
+    }
+
     public Node multiply(double scalar) {
         return new Node(weight * scalar, bias * scalar);
     }
