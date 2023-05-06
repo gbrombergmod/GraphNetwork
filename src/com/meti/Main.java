bda371d6792f92e054721ab0c673c7a3fcb4282d
SirMathhman <sirmathhman@gmail.com>
2023-05-06 19:20:51 -0400

    Add some meaningful output

--- src/com/meti/Main.java
+++ src/com/meti/Main.java
@@ -1,5 +1,6 @@
 package com.meti;
 
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -17,15 +18,13 @@ public class Main {
         var node = Node.random();
         System.out.println(node);
 
-        data.entrySet()
+        var trained = data.entrySet()
                 .stream()
                 .collect(Collectors.groupingBy(entry -> entry.getKey() % BATCH_COUNT))
                 .values().stream().reduce(node, (node1, batch) -> {
                     var gradientSum = batch.stream().reduce(Node.zero(), (gradientSum1, entry) -> {
-                        var key = (double) entry.getKey();
-                        var input = key / (double) data.size();
-                        var evaluated = node1.weight * input + node1.bias;
-                        var activated = 1d / (1d + Math.pow(Math.E, -evaluated));
+                        var input = normalize(data, entry);
+                        var activated = node1.forward(input);
 
                         var isEven = entry.getValue();
                         var expected = isEven ? 0d : 1d;
@@ -41,6 +40,27 @@ public class Main {
                     System.out.println(newNode);
                     return newNode;
                 }, Main::selectRight);
+
+        var totalCorrect = data.entrySet().stream().mapToInt(entry -> {
+            var input = normalize(data, entry);
+            var result = trained.forward(input);
+            var isEven = entry.getValue();
+            if (isEven && result < 0.5) {
+                return 1;
+            } else if (!isEven && result >= 0.5) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }).sum();
+
+        var percentage = (double) totalCorrect / (double) data.size();
+        System.out.println((percentage * 100) + "%");
+    }
+
+    private static double normalize(Map<Integer, Boolean> data, Map.Entry<Integer, Boolean> entry) {
+        var key = (double) entry.getKey();
+        return key / (double) data.size();
     }
 
     private static Node selectRight(Node node, Node node1) {
@@ -57,6 +77,11 @@ public class Main {
             var weight = Math.random();
             var bias = Math.random();
             return new Node(weight, bias);
+        }
+
+        private double forward(double input) {
+            var evaluated = this.weight * input + this.bias;
+            return 1d / (1d + Math.pow(Math.E, -evaluated));
         }
 
         public Node multiply(double scalar) {
